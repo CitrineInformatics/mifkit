472b351da3ed3d0a512ead4605a5fdff5e9ac16a
Kyle <kyle@citrine.io>
2015-08-07 17:05:41 -0700

    add url to setup

--- setup.py
+++ setup.py
@@ -2,6 +2,7 @@ from setuptools import setup, find_packages
 
 setup(name='Mifkit',
       version='1.0.0',
+      url='http://github.com/CitrineInformatics/mifkit',
       description='Tools for working with the Materials Information File (MIF).',
       author='Kyle Michel',
       author_email='kyle@citrine.io',
